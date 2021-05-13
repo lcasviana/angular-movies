e73019f5f5a965958f59c5a4117cb5e9672808f9
Lucas Viana <lcasviana@gmail.com>
2021-05-12 21:42:03 -0300

    Improving layout

--- src/app/movies/components/movie/movie.component.ts
+++ src/app/movies/components/movie/movie.component.ts
@@ -4,6 +4,7 @@ import { TheMoviesDbResultModel } from "../../models/the-movies-db.model";
 @Component({
   selector: "movie",
   templateUrl: "./movie.component.html",
+  styleUrls: ["./movie.component.scss"],
 })
 export class MovieComponent {
   @Input() movie!: TheMoviesDbResultModel;
